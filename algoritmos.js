d9646c237d61471e4d58c94651beabd590f93905
albertplacreditas <albert.pla@creditas.com>
2020-08-11 15:38:51 +0200

    [ADDED] first exercise

--- algoritmos.js
+++ algoritmos.js
@@ -1,1 +1,13 @@
-//Algoritmos JS
+'use-strict'
+
+function palindrome(word) {
+
+    cleanWord=word.replace(/[^a-zA-Z]+/g,'').toLowerCase()
+    reverseWord=cleanWord.split('').reverse().join('')
+
+    return cleanWord === reverseWord
+}
+
+var resultado= palindrome("rec!!)on23.oCER")
+console.log(resultado) 
+
