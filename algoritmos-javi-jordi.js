c8667771e6c6bbe075b013c188f5e66d562d01e0
jordi-gomis <jordi.gomis@creditas.com>
2020-08-11 18:34:17 +0200

    try resolve anagram

--- algoritmos-javi-jordi.js
+++ algoritmos-javi-jordi.js
@@ -19,7 +19,6 @@ function countWords(str) {
         else {
             result[word] = 1
         }
-        
     })
     return result;
 }
@@ -35,11 +34,42 @@ function countWords(str) {
         else {
             result[word] = 1
         }
-        
     })
     return result;
 }
 
+function anagram(words) {
+    var lowerWords = words.map(word => {
+        return word.replace(/[^a-zA-Z]+/,'').toLowerCase()
+    })
+
+    counterLeters = []
+    lowerWords.map((word)=>{
+        singleLetersArray = word.split('')
+        var leters = {}
+        singleLetersArray.map(leter => {
+            if(leters[leter]){
+                leters[leter]++
+            }else{
+                leters[leter] = 1
+            }
+        })
+        counterLeters[word] = leters
+    })
+
+    result = []
+    for (var item in counterLeters){
+        for (var x in item){
+            console.log(x);
+        }
+    }
+
+
+    return counterLeters
+}
+
+
 resultado1 = palindrome("rec!!)on23.oCER")
 console.log("Ejercicio 1 : " + resultado1) 
 console.log(countWords("Doo bee doo bee doo"))
+console.log(anagram(["Paco", "capo", "Casa", "saCa", "caas", "chopO", "Pocho", "perro"]))
