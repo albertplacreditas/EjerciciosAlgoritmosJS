234034154a0e3b90022128126bbb27f2a9870586
lluis-Antoni <lluis.antoni@creditas.com>
2020-08-11 16:32:39 +0200

    Ejercicio 2 terminado

--- algoritmos.js
+++ algoritmos.js
@@ -1,5 +1,7 @@
 'use-strict'
 
+//Ejercicio 1
+
 function palindrome(word) {
 
     cleanWord=word.replace(/[^a-zA-Z]+/g,'').toLowerCase()
@@ -9,5 +11,24 @@ function palindrome(word) {
 }
 
 var resultado= palindrome("rec!!)on23.oCER")
-console.log(resultado) 
+//console.log(resultado) 
 
+//Ejercicio 2
+
+function countWords(text){
+    cleanText=text.replace(/[^a-zA-Z ]+/g,'').toLowerCase()
+    arrayText=cleanText.split(' ')
+    dictionary = {}
+    arrayText.map((word)=>{
+        if(!dictionary[word]){
+            dictionary[word] = 0
+        }
+        dictionary[word] += 1
+    })
+
+    return dictionary
+}
+
+//console.log(countWords('Doo bee doo bee doo'))
+
+//Ejercicio 3
