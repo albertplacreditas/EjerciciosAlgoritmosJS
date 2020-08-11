c6843eba40a3be4b54d39862405b67aaf228aaa3
javiCervello <javi.cervello@gmail.com>
2020-08-11 16:17:09 +0200

    [Added] second exercise done

---
+++ algoritmos-javi-jordi.js
@@ -0,0 +1,29 @@
+'use-strict'
+
+function palindrome(word) {
+
+    cleanWord=word.replace(/[^a-zA-Z]+/g,'').toLowerCase()
+    reverseWord=cleanWord.split('').reverse().join('')
+
+    return cleanWord === reverseWord
+}
+function countWords(str) {
+    //sentence = str.replace(/[^a-zA-Z]+/,'').toLowerCase()
+    words = str.toLowerCase().split(" ")
+    result = {}
+    
+    words.map((word)=>{
+        if(result[word]){
+            result[word]++
+        }
+        else {
+            result[word] = 1
+        }
+        
+    })
+    return result;
+}
+
+resultado1= palindrome("rec!!)on23.oCER")
+console.log("Ejercicio 1 : " + resultado1) 
+console.log(countWords("Doo bee doo bee doo"))
