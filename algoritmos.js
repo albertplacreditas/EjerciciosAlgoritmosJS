1127c9837eb51bab30953638e66b694e3ef56605
lluis-Antoni <lluis.antoni@creditas.com>
2020-08-12 09:48:13 +0200

    ejercicio 3

--- algoritmos.js
+++ algoritmos.js
@@ -32,3 +32,26 @@ function countWords(text){
 //console.log(countWords('Doo bee doo bee doo'))
 
 //Ejercicio 3
+
+function anagram(words) {
+    var groupWords=[];
+    var messyWords =[];
+
+    var lowerWords = words.map(word => {
+        return word.replace(/[^a-zA-Z]+/,'').toLowerCase();
+    })
+
+    lowerWords.map((word,index)=>{
+        sortedWord = word.split('').sort().join('')
+        indexWord = messyWords.indexOf(sortedWord) 
+        if(indexWord === -1){
+            groupWords.push([words[index]])
+            messyWords.push(sortedWord)
+        }else{
+            groupWords[indexWord].push(words[index])
+        }
+    })
+    
+    return groupWords
+}
+console.log(anagram(["Paco", "capo", "Casa", "saCa", "caas", "chopO", "Pocho", "perro"]))
