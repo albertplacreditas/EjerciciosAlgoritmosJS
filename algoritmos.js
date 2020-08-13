a1452b19186bd5cfc2d213c4f106d8055bc5137d
albertplacreditas <albert.pla@creditas.com>
2020-08-13 10:58:59 +0200

    [ADDED] Exercise 1-5

--- algoritmos.js
+++ algoritmos.js
@@ -1,6 +1,6 @@
 'use-strict'
 
-//Ejercicio 1
+//Ejercicio 1 - Palindrome
 
 function palindrome(word) {
 
@@ -13,7 +13,7 @@ function palindrome(word) {
 var resultado= palindrome("rec!!)on23.oCER")
 //console.log(resultado) 
 
-//Ejercicio 2
+//Ejercicio 2 - Count Words
 
 function countWords(text){
     cleanText=text.replace(/[^a-zA-Z ]+/g,'').toLowerCase()
@@ -31,7 +31,7 @@ function countWords(text){
 
 //console.log(countWords('Doo bee doo bee doo'))
 
-//Ejercicio 3
+//Ejercicio 3 - Anagram
 
 function anagram(words) {
     var groupWords=[];
@@ -55,3 +55,49 @@ function anagram(words) {
     return groupWords
 }
 console.log(anagram(["Paco", "capo", "Casa", "saCa", "caas", "chopO", "Pocho", "perro"]))
+
+//Ejercicio 4 - Running Time of Algorithms
+
+function runningTime(arr) {
+    let changes=0
+    let i, key, j
+    for(let i=0;i < arr.length; i++){
+        key = arr[i]
+        j = i - 1
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j]
+            j = j - 1
+            changes++
+        }
+        arr[j + 1] = key
+        console.log(arr)
+    }
+    return changes
+}
+
+/* let totalChanges= 0;
+    for(let i=0;i < arr.length; i++){
+        for(let j=0;j < arr.length; j++){
+                let aux = arr[i];
+            if(arr[i]>arr[j] && i<j){
+                arr[i] = arr[j]
+                arr[j] = aux
+                totalChanges ++
+            }
+        }
+        console.log(arr);
+    }
+    return totalChanges */
+
+//Ejercicio 5 - Save the Prisoner!
+
+function saveThePrisoner(n,m,s) {
+    var ultimoprisionero = ((m % n) + (s-1)) % n;
+    return ultimoprisionero
+}
+
+// Ejercicio 6 - Missing Numbers
+
+// Ejercicio 7 - Halloween Sale
+
+// Ejercicio 8 - KnightL on a Chessboard
